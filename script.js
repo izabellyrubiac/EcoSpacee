3aabef3b1e2ff3e19f5ab1c200e20189cebe4221
Izabelly Rubia Costa <izabellyrubic@gmail.com>
2025-06-13 12:44:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function validar()
+{
+    var login = document.getElementById("login").value;
+    var senha = document.getElementById("senha").value;
+
+    if (login == "admin@fortes.com.br" && senha == "123456")
+    {
+        location.href = "entrar.html"
+    }
+    else if (login == "cooperativas@fortes.com.br" && senha == "123456")
+    {
+        location.href = "entrar-coop.html"
+    }
+    else 
+    {
+        alert('Dados inválidos')
+    }
+    }
+
+function criar()
+{
+    location.href = "criar.html"
+}
+
